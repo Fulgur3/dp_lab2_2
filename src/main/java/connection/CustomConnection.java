53a1fd29ab2bf1e1cfa1cf6272cbdfbe8be21320
Fulgur3 <belokrsasha@gmail.com>
2022-12-01 02:24:00 +0200

    2.2 lab ready

--- src/main/java/connection/CustomConnection.java
+++ src/main/java/connection/CustomConnection.java
@@ -1,2 +1,36 @@
-package connection;public class CustomConnection {
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CustomConnection {
+    private static Connection connection = null;
+    public static Statement statement=null;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/dp";
+    private static final String USER = "root";
+    private static final String PASSWORD = "5661369";
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
